82aa74bef15e5641f3052f5ee5bccf3ca4842a0c
Héctor Molinero Fernández <hector@molinero.dev>
2021-12-08 17:45:03 +0100

    0.2.0

--- bin/demergi.js
+++ bin/demergi.js
@@ -84,7 +84,7 @@ getopts: for (let i = 0; i < argv.length; i++) {
       break;
     case "-v":
     case "--version":
-      console.log(`Demergi 0.1.4`);
+      console.log(`Demergi 0.2.0`);
       process.exit(0);
       break;
     case "-h":
