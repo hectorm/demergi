90bdab22e28f5cd7b8e6fa1ce832c3f898e4c4e1
Héctor Molinero Fernández <hector@molinero.dev>
2021-12-12 13:06:31 +0100

    Fix missing rename

--- bin/demergi.js
+++ bin/demergi.js
@@ -234,7 +234,7 @@ getopts: for (let i = 0; i < argv.length; i++) {
     });
 
     await proxy.start();
-    console.log(`Listening on ${proxy.host}:${proxy.port}`);
+    console.log(`Listening on ${proxy.addr}:${proxy.port}`);
 
     process.on("SIGINT", async () => await proxy.stop());
     process.on("SIGTERM", async () => await proxy.stop());
